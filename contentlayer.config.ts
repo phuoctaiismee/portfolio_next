8613eab657a680f22d8ad018e1802a3c51eb29fb
phuoctaiismee <113773744+phuoctaiismee@users.noreply.github.com>
2023-12-25 01:18:52 +0700

    done code ui project page

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,15 +1,106 @@
-import { defineDocumentType, makeSource } from 'contentlayer/source-files'
+import {
+    defineDocumentType,
+    ComputedFields,
+    makeSource,
+} from 'contentlayer/source-files'
+import remarkGfm from 'remark-gfm'
+import rehypePrettyCode from 'rehype-pretty-code'
+import rehypeSlug from 'rehype-slug'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import { DocumentMeta } from '@contentlayer/core'
 
-export const Post = defineDocumentType(() => ({
-    name: 'Post',
-    filePathPattern: `**/*.md`,
+const computedFields: ComputedFields = {
+    path: {
+        type: 'string',
+        resolve: (doc) => `/${doc._raw.flattenedPath}`,
+    },
+    slug: {
+        type: 'string',
+        resolve: (doc) => doc._raw.flattenedPath.split('/').slice(1).join('/'),
+    },
+}
+
+export const Project = defineDocumentType(() => ({
+    name: 'Project',
+    filePathPattern: './projects/**/*.mdx',
+    contentType: 'mdx',
+
     fields: {
-        title: { type: 'string', required: true },
-        date: { type: 'date', required: true },
+        title: {
+            type: 'string',
+            required: true,
+        },
+        description: {
+            type: 'string',
+            required: true,
+        },
+        date: {
+            type: 'date',
+        },
+        url: {
+            type: 'string',
+        },
+        published: {
+            type: 'boolean',
+        },
+        repository: {
+            type: 'string',
+        },
     },
-    computedFields: {
-        url: { type: 'string', resolve: (post) => `/post/${post._raw.flattenedPath}` },
-    },
+    computedFields,
 }))
 
-export default makeSource({ contentDirPath: 'posts', documentTypes: [Post] })
+export const Page = defineDocumentType(() => ({
+    name: 'Page',
+    filePathPattern: 'pages/**/*.mdx',
+    contentType: 'mdx',
+    fields: {
+        title: {
+            type: 'string',
+            required: true,
+        },
+        description: {
+            type: 'string',
+        },
+    },
+    computedFields,
+}))
+
+export default makeSource({
+    contentDirPath: './content',
+    documentTypes: [Page, Project],
+    mdx: {
+        remarkPlugins: [remarkGfm],
+        rehypePlugins: [
+            rehypeSlug,
+            [
+                rehypePrettyCode,
+                {
+                    theme: 'github-dark',
+                    onVisitLine(node) {
+                        // Prevent lines from collapsing in `display: grid` mode, and allow empty
+                        // lines to be copy/pasted
+                        if (node.children.length === 0) {
+                            node.children = [{ type: 'text', value: ' ' }]
+                        }
+                    },
+                    onVisitHighlightedLine(node) {
+                        node.properties.className.push('line--highlighted')
+                    },
+                    onVisitHighlightedWord(node) {
+                        node.properties.className = ['word--highlighted']
+                    },
+                },
+            ],
+            [
+                rehypeAutolinkHeadings,
+                {
+                    properties: {
+                        className: ['subheading-anchor'],
+                        ariaLabel: 'Link to section',
+                    },
+                },
+            ],
+        ],
+    },
+})
