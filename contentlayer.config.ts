cd43322fc2d749e5db123151490db66730981172
phuoctaiismee <113773744+phuoctaiismee@users.noreply.github.com>
2023-12-25 01:33:50 +0700

    Update contentlayer.config.ts

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -72,35 +72,9 @@ export default makeSource({
     mdx: {
         remarkPlugins: [remarkGfm],
         rehypePlugins: [
+             rehypeAutolinkHeadings,
+            rehypePrettyCode,
             rehypeSlug,
-            [
-                rehypePrettyCode,
-                {
-                    theme: 'github-dark',
-                    onVisitLine(node) {
-                        // Prevent lines from collapsing in `display: grid` mode, and allow empty
-                        // lines to be copy/pasted
-                        if (node.children.length === 0) {
-                            node.children = [{ type: 'text', value: ' ' }]
-                        }
-                    },
-                    onVisitHighlightedLine(node) {
-                        node.properties.className.push('line--highlighted')
-                    },
-                    onVisitHighlightedWord(node) {
-                        node.properties.className = ['word--highlighted']
-                    },
-                },
-            ],
-            [
-                rehypeAutolinkHeadings,
-                {
-                    properties: {
-                        className: ['subheading-anchor'],
-                        ariaLabel: 'Link to section',
-                    },
-                },
-            ],
         ],
     },
 })
