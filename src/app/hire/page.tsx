3eb952057a0c78a764861c523b9c63778cd9bf65
phuoctaiismee <113773744+phuoctaiismee@users.noreply.github.com>
2023-12-31 04:38:58 +0700

    Update page.tsx

--- src/app/hire/page.tsx
+++ src/app/hire/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 import React from 'react'
 import {useTheme} from "next-themes";
-import {MoonIcon, SunIcon} from "lucide-react";
 import ThemeSwitcher from "@/components/ThemeSwitcher/ThemeSwitcher";
 
 const Hire = () => {
